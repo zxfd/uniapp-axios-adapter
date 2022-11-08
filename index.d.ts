7f7076b8c5ade0227e8f568cc62b3ab5f9e70f63
周小富 <zhou.xiaofu@tslsmart.com>
2022-11-08 15:05:51 +0800

    feat(index.d.ts): ✨ 新增类型声明文件,支持ts

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import type { AxiosStatic, AxiosPromise, AxiosRequestConfig } from "axios";
+
+export declare function UniAdapter(config: AxiosRequestConfig): AxiosPromise
+
+
+declare const axios: AxiosStatic;
+
+export default axios
